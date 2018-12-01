b8f28d4a924b6b0f5cdcaba7212e3aa14f02de37
Toshiki Ohnogi <thetoshiki0419@gmail.com>
2018-12-01 14:40:14 +0900

    メキシコ人クラス

---
+++ mexicanpeople.py
@@ -0,0 +1,17 @@
+import pyxel
+
+
+class MexicanPeople:
+
+  def __init__(self, xIdx, y):
+
+    self.position = [25, 30, 35, 40]
+
+    self.x = self.position[xIdx]
+    self.y = y
+  
+  def update(self, xIdx):
+    self.x = self.position[xIdx]
+  
+  def draw(self):
+    pyxel.blt(self.x, self.y, 0, 1, 16, 7, 8)
